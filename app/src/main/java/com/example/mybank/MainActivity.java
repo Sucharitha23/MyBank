c716f4d4753b7d83bde896423d8fa40e4b19a566
Sucharitha <sucharithapillala@gmail.com>
2021-06-17 22:50:01 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mybank/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mybank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView customer,transaction;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        customer=findViewById(R.id.customer);
+        transaction=findViewById(R.id.transactions);
+
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,user_list.class));
+            }
+        });
+        transaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,history_list.class));
+            }
+        });
+    }
+}
