e89d699abc422b90cce7ef61829a80d0cddbf75a
Sucharitha <sucharithapillala@gmail.com>
2021-06-18 12:49:31 +0530

    Initial Commit

--- app/src/main/java/com/example/mybank/MainActivity.java
+++ app/src/main/java/com/example/mybank/MainActivity.java
@@ -8,13 +8,14 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView customer,transaction;
+    ImageView customer,transaction,syllabus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         customer=findViewById(R.id.customer);
         transaction=findViewById(R.id.transactions);
+        syllabus = findViewById(R.id.syllabus);
 
         customer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +29,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,history_list.class));
             }
         });
+        syllabus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this,user_list.class ));
+            }
+        });
+
     }
 }
